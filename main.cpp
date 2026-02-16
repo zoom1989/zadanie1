ad8cfa995dd566fc8b985f1aeae5c9f2ca297e89
zoom1989 <shashkov0688@gmail.com>
2026-02-16 12:59:55 +0300

    «Переменные и их типы» задание 1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    // Запрашиваем число у пользователя
+    cout << "100 " << endl;
+    cin >> number;
+
+    // Выводим введенное число
+    cout << "100 " << endl;
+    cout << number << endl;
+
+    return 0;
+}
