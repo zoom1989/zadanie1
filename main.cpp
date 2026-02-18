bd0305f62c6b3469ef0718b7c007fd2211cb37e7
zoom1989 <shashkov0688@gmail.com>
2026-02-18 11:23:28 +0300

    Dorabotka

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-using namespace std;
 
 int main() {
     int number;
 
-    // Запрашиваем число у пользователя
-    cout << "100 " << endl;
-    cin >> number;
+    std::cout << " 957 " << std::endl;
+    std::cin >> number;
 
-    // Выводим введенное число
-    cout << "100 " << endl;
-    cout << number << endl;
+    std::cout << "Вы ввели: " << std::endl;
+    std::cout << number << std::endl;
 
     return 0;
 }
